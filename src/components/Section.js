2b3bff95224dc68cd18ee91a57f37710a35e32ee
ilia-puchkov <112614894+ilia-puchkov@users.noreply.github.com>
2023-01-26 09:59:05 +0300

    FIX: S8 R2

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,9 +1,9 @@
 class Section {
   // Конструктор класса
-  constructor({items, renderer}, containerSelector) {
+  constructor({items, renderer}, container) {
     this._items = items;
     this._renderer = renderer;
-    this._container = containerSelector;
+    this._container = container;
   }
 
   // Отрисовка
